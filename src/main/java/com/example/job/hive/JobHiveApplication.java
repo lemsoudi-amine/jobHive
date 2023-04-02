a6ecb5107eb4ba71542930040a44488e34b59bbd
mohamed-amine lemsoudi <amine.lemsoudi@gmail.com>
2023-04-03 00:08:56 +0200

    initial commit

---
+++ src/main/java/com/example/job/hive/JobHiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.job.hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/job/hive/JobHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job.hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
